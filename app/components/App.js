6a5029c54b738d9332c01f1c013ae9993a098cc8
mikejonas <motion@gmail.com>
2016-07-11 01:48:38 -0700

    Install superagent and lodash

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import request from 'superagent';
+import _map from 'lodash/map';
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {test: 'foo'};
+    this.state = {something: true};
   }
+
+  componentDidMount() {
+
+  }
+
+
   render() {
     return (
       <div className='container'>
